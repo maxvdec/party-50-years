34c54226cba91c350271dc56fa5d44b24f9516ce
Max Van den Eynde <maxvdec@gmail.com>
2025-05-11 19:13:00 +0200

    release

--- src/routes/cathegories.ts
+++ src/routes/cathegories.ts
@@ -4,9 +4,15 @@ export interface Cathegory {
 }
 
 export const cathegories: Cathegory[] = [
-	{ name: 'El que porta asdfasdf', id: '1' },
-	{ name: 'El que és tonto', id: '2' },
-	{ name: 'Abduskan', id: '3' },
-	{ name: 'El que té un pèl de gatsby', id: '4' },
-	{ name: 'El que té un pèl de gatsby', id: '5' }
+	{ name: 'Li va renyar la Tina Turner', id: '1' },
+	{ name: 'Ha creat el còctel Fernandez Sunrise', id: '2' },
+	{ name: 'Ha jugat al Barça de Futbol', id: '3' },
+	{ name: 'És profesional de modelatge amb clics de Famobil', id: '4' },
+	{ name: 'Fa màgia', id: '5' },
+	{ name: 'Ha ballat amb la Roser i un xaman a Eivissa', id: '6' },
+	{ name: 'És expert en criar piranyes', id: '7' },
+	{ name: "S'ha passat mig any excavant a la selva de Bolívia", id: '8' },
+	{ name: 'Ha estat professor de Princeton', id: '9' },
+	{ name: 'Ha viscut uns anys a Honk Kong', id: '10' },
+	{ name: 'Persones de Puerto Rico i Colòmbia que parlen el Català', id: '11' }
 ];
